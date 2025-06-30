3d68a36a7a0c5ea707658f498bca1e9d3c031f7b
Rahul <rahulagse12345@gmail.com>
2025-06-30 20:23:16 +0530

    trigger redeploy

--- SocketIO/server.js
+++ SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://chatgram-3-2fco.onrender.com",
+    origin: "http://localhost:3001",
     methods: ["GET", "POST"],
   },
 });
